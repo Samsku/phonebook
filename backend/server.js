bee3d951d71f688ad7039089c1b1a396d17368c2
Samsku <turunensami54@gmail.com>
2025-10-24 12:58:00 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -71,6 +71,7 @@ if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "build", "index.html"))
     })
+
 }
 
 const PORT = process.env.PORT || 3001
