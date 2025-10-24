0a74e4a01f9bc48fc361232fb3c916bcbd76b465
Samsku <turunensami54@gmail.com>
2025-10-24 13:00:10 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -64,15 +64,5 @@ app.delete("/api/persons/:id", (req, res) => {
     res.status(204).end()
 })
 
-// Serve frontend build in production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "build")))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "build", "index.html"))
-    })
-
-}
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
